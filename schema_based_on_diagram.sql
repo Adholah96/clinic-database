67721aa0acfece0b1dddd9083081f91c4ebd1636
QueenterJuma <jumahqueenter@gmail.com>
2023-05-10 10:51:56 +0300

    Create join table and indexes

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -33,3 +33,18 @@ create table invoice_items(id int primary key generated always as identity,
         treatment_id int references treatments(id)
  );
 
+--  Join tables from many-to-many reletionships
+
+create table medical_history_treatment(
+    id int generated always as identity primary key, 
+    medical_history_id int references medical_histories(id), 
+    treatment_id int references treatments(id)
+);
+
+-- Add indexes
+create index patient_id_idx on medical_histories(patient_id);
+create index medical_history_id_idx on invoices(medical_history_id);
+create index invoice_id_idx on invoice_items(invoice_id);
+create index treatment_id_idx on invoice_items(treatment_id);
+create index medical_history_treatments_id_idx on medical_history_treatment(medical_history_id);
+create index medical_histor_treatments_two_id_idx on medical_history_treatment(treatment_id);
